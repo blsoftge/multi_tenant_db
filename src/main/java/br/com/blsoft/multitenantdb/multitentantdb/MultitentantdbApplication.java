fe2a5da928ab49668dcaa04c467f72f1caccabff
brunolopeschagas <brunolopeschagas@gmail.com>
2022-03-17 15:17:12 -0300

    first commit

---
+++ src/main/java/br/com/blsoft/multitenantdb/multitentantdb/MultitentantdbApplication.java
@@ -0,0 +1,13 @@
+package br.com.blsoft.multitenantdb.multitentantdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitentantdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultitentantdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/blsoft/multitenantdb/multitentantdb/MultitentantdbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.blsoft.multitenantdb.multitentantdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitentantdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
