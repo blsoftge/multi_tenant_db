1047fbf9a0c6a331f10360e5b8ea000b449c14ce
brunolopeschagas <brunolopeschagas@gmail.com>
2022-03-17 18:10:32 -0300

    projeto iniciando

---
+++ src/main/java/br/com/blsoft/multitenantdb/multitentantdb/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package br.com.blsoft.multitenantdb.multitentantdb.config;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+        config.addAllowedMethod(HttpMethod.PUT);
+        config.addAllowedMethod(HttpMethod.DELETE);
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter());
+        bean.setOrder(0);
+        return bean;
+    }
+}
